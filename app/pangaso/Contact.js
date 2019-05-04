e8f8f57024aa8163f95813b9ed39e4847962f070
Kati Frantz <bahdcoder@gmail.com>
2019-05-04 21:21:40 +0100

    add pangaso folder to vc

---
+++ app/pangaso/Contact.js
@@ -0,0 +1,31 @@
+const {
+    ID,
+    Text,
+    Resource,
+    HasOne,
+    HasOneEmbedded,
+} = require('../../dist/server/main')
+
+class Contact extends Resource {
+    displayValue() {
+        return 'firstName'
+    }
+
+    fields() {
+        return [
+            ID.make('ID'),
+            Text.make('First Name').createWithRules('required|max:40').searchable(),
+            Text.make('Last Name').createWithRules('required|max:40').searchable(),
+            Text.make('Email').createWithRules('required|email').searchable(),
+            Text.make('Phone').createWithRules('required').searchable(),
+            HasOneEmbedded.make('Address').withFields([
+                Text.make('City').createWithRules('required'),
+                Text.make('State').createWithRules('required'),
+                Text.make('Postal Code').createWithRules('required')
+            ]),
+            HasOne.make('Organisation').searchable()
+        ]
+    }
+}
+
+module.exports = Contact
