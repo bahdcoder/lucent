fa6976474f3f88a54c00e9cf0901567bad07cfe0
Frantz Kati <bahdcoder@gmail.com>
2020-02-21 04:19:34 +0100

    Fix capital import

--- app/index.js
+++ app/index.js
@@ -2,13 +2,13 @@ const { Lucent } = require(process.env.CI_ENVIRONMENT
     ? '../dist/server/main'
     : 'lucent-admin')
 
-process.env.MONGO_URL = 'mongodb://localhost/lucent-admin-3'
+process.env.MONGO_URL = 'mongodb://localhost/lucent-admin-4'
 
 new Lucent()
     .onPort(process.env.PORT || 3004)
     .mongo(
         process.env.MONGO_URL ||
             'mongodb://pangaso:pangaso1@ds149596.mlab.com:49596/pangaso',
-        process.env.DATABASE || 'lucent-admin-3'
+        process.env.DATABASE || 'lucent-admin-4'
     )
     .start()
